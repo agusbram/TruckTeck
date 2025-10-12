cea15f6640b88bf9dbd7bcc57c8efc0c756d6e7f
Tobias Funes <tobiasfunes@hotmail.com.ar>
2025-10-12 15:23:40 -0300

    Backend Spring Boot

---
+++ src/main/java/ar/edu/iua/TruckTeck/TruckTeckApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.iua.TruckTeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckTeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckTeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/iua/TruckTeck/TruckTeckApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.iua.TruckTeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckTeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
